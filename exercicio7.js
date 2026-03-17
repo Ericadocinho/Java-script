ca686b48b53e5331b05ad19d0cf819db122dc2b0
Ericadocinho <erica.enri25@gmail.com>
2026-03-16 22:18:09 -0300

    Add files via upload

---
+++ exercicio7.js
@@ -0,0 +1,4 @@
+let notas = [20, 12, 55];
+
+
+console.log (notas[0] + notas[1] + notas[2]);
---
+++ exercicio8.js
@@ -0,0 +1,28 @@
+let somarNumeros = [24, 70, 48] 
+let maior = somarNumeros [0]
+  for ( let i = 0; i < 3; i++) {
+    if ( somarNumeros [ i ] > maior) { 
+        maior = somarNumeros [i]
+    }
+  }
+  console.log ( maior)
+
+
+
+
+
+
+
+
+
+
+
+// let maior = somarNumeros
+// let numero1 = somarNumeros [0]
+// let numero2 = somarNumeros [1]
+
+// if (numero1 >= numero2) { 
+//     maior = numero1
+// } 
+
+// console.log ( maior) 
